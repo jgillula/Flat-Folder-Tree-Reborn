8a8bf9e86f7373d3ee91453ec05c2df97363fa86
Jeremy Gillula <jgillula@gmail.com>
2019-06-17 17:49:28 -0700

    Forgot to fix the version number, so now we pull it directly from the install.rdf file

--- make-xpi-package.sh
+++ make-xpi-package.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# First get the version number directly from the changelog
-version_number=`head -n 1 CHANGELOG.md | sed 's/# //'`
+# First get the version number directly from the install.rdf file
+version_number=`sed -n -e 's|.*<em:version>\(.*\)</em:version>|\1|p' src/install.rdf`
 
 # Go into the source directory...
 cd src
+
+# Delete any extraneous files
+find . -name *~ -delete
 
 # And make the xpi file by zipping stuff up!
 zip -r ../flat_folder_tree_reborn-$version_number-tb.xpi *
